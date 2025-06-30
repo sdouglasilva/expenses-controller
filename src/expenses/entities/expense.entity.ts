ac6783b234ec4c1debdd4218ce9043c0671e973b
Douglas Silva <douglas.ernesto.silva@gmail.com>
2025-06-29 23:00:42 -0300

    feat(database): Reconfigurando TypeORM e entidades User/Expense

--- src/expenses/entities/expense.entity.ts
+++ src/expenses/entities/expense.entity.ts
@@ -1,1 +1,21 @@
-export class Expense{}
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
+import { User } from "src/users/entities/user.entities";
+
+@Entity('expenses')
+export class Expense{
+  @PrimaryGeneratedColumn('uuid'
+  )
+  id: string;
+  @Column({type:'date'})
+  date: Date;
+
+  @Column({type:'decimal', precision:10, scale:2})
+  value:number
+
+  @ManyToOne(()=>User, (user)=> user.expenses,{onDelete:'CASCADE'}
+  )
+  @JoinColumn({name:'userId'})
+  user: User;
+  @Column({nullable:false})
+  userId: string;
+}
--- src/users/entities/user.entities.ts
+++ src/users/entities/user.entities.ts
@@ -1,1 +1,16 @@
-export class User{}
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Expense } from "src/expenses/entities/expense.entity";
+
+@Entity("users")
+export class User{
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({unique:true})
+  email:string;
+
+  @Column()
+  password: string
+  @OneToMany(()=> Expense,(expense)=> expense.user)
+  expenses: Expense[]
+}
